24524c6012c862be6a336a4cc0a23241d7c6a511
david kuseh <davegru203@gmail.com>
2021-06-04 18:25:14 +0000

    add products to commercejs product list

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,24 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { 
-        id: 1, 
-        name: 'Nvidia RTX 3090', 
-        description: 'Brand new MSI RTX 3090 GPU', 
-        price: '$1499.99', 
-        image: 'https://www.hardwarecooking.fr/wp-content/uploads/2020/11/test-msi-rtx-3090-gaming-x-trio-05-1536x1024.jpg'
-     },
-    { 
-        id: 2, 
-        name: 'Intel i9 10900K', 
-        description: 'Intel Core i9-10900K CPU', 
-        price: '$529.99', 
-        image: 'https://i1.wp.com/www.funkykit.com/wp-content/uploads/2020/05/Intel-10900K-2.jpg?fit=1280%2C720&ssl=1'
-    }
-]
-
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
 
     return (
